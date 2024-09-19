1745a4801949083645a01006a0b5a7895445a474
alanbebermartins <abebermartins@gmail.com>
2024-09-19 14:58:09 -0300

    Resolvido questão 2

--- Resolução Desafio SQL.sql
+++ Resolução Desafio SQL.sql
@@ -2,3 +2,7 @@
 
 -- QUESTÃO 1 - Buscar o nome e ano dos filmes
 SELECT * FROM Filmes
+
+-- QUESTÃO 2 - Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
+
+SELECT * FROM Filmes ORDER BY Ano ASC
