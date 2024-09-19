1d412953f4ada1d7012d6b5b7333e209f2883f9e
alanbebermartins <abebermartins@gmail.com>
2024-09-19 15:03:22 -0300

    Resolvido questão 3

--- Resolução Desafio SQL.sql
+++ Resolução Desafio SQL.sql
@@ -6,3 +6,10 @@ SELECT * FROM Filmes
 -- QUESTÃO 2 - Buscar o nome e ano dos filmes, ordenados por ordem crescente pelo ano
 
 SELECT * FROM Filmes ORDER BY Ano ASC
+
+-- QUESTÃO 3 - Buscar pelo filme de volta para o futuro, trazendo o nome, ano e a duração
+-- MANEIRA 1
+SELECT * FROM Filmes WHERE Nome = 'De Volta para o Futuro'
+
+-- MANEIRA 2
+SELECT Nome, Ano, Duracao FROM Filmes WHERE Nome = 'De Volta para o Futuro'
